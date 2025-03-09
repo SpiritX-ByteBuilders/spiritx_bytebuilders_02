b9d46023738fcdfc01bd9b5092369173ef3e50c8
Amri <muhammedamri2002@gmail.com>
2025-03-09 15:16:03 +0530

    Implemented the frontend for tournament summary

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -4,16 +4,32 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Players from "./pages/Players";
 import PlayerProfile from "./pages/PlayerProfile";
+import TournamentSummary from "./pages/TournamentSummary";
 
 const App = () => {
   const [user, setUser] = useState(null);
 
   return (
     <Routes>
-      <Route path="/" element={user ? <Navigate to="/players" /> : <Login onLogin={setUser} />} />
+      <Route
+        path="/"
+        element={
+          user ? <Navigate to="/players" /> : <Login onLogin={setUser} />
+        }
+      />
       <Route path="/register" element={<Register onRegister={setUser} />} />
-      <Route path="/players" element={user ? <Players /> : <Navigate to="/" />} />
-      <Route path="/players/:id" element={user ? <PlayerProfile /> : <Navigate to="/" />} />
+      <Route
+        path="/players"
+        element={user ? <Players /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/players/:id"
+        element={user ? <PlayerProfile /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/tournament-summary"
+        element={user ? <TournamentSummary /> : <Navigate to="/" />}
+      />
     </Routes>
   );
 };
